c848be4ee1b0120cffa0899675acd511adf718fe
6missedcalls <supremehitsheads@gmail.com>
2025-05-31 00:11:15 -0700

    i remember i hate this shit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import Button from "@/components/ui/Button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-neutral-950">
-      <Button 
-        color="primary" 
-        size="large"
-      >
-        Click me
-      </Button>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-neutral-0">
+      <div className="flex flex-col gap-4">
+        <Button 
+          color="secondary"
+          size="large"
+        >
+          Click me
+        </Button>
+      </div>
     </div>
   );
 }
