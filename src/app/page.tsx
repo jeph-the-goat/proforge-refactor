7bc11232bd7f34272691d68712925a4f9c2b447f
6missedcalls <supremehitsheads@gmail.com>
2025-06-01 17:07:07 -0700

    added logo boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { useState } from "react";
-import Checkbox from "@/components/ui/Checkbox";
+import Logo from "@/components/ui/Logo";
 
 export default function Home() {
   const [isChecked, setIsChecked] = useState(false);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-neutral-0">
-      <Checkbox size="sm" isChecked={isChecked} onChange={setIsChecked} />
+      <Logo type="icon" />
+      <Logo type="icon-text" />
     </div>
   );
 }
