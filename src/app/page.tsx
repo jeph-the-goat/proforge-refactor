a6d5aaa4c340edfed88f45561efb61f41e12fa8d
6missedcalls <supremehitsheads@gmail.com>
2025-06-01 17:00:57 -0700

    checkboxes done and this is weirdly calming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Button from "@/components/ui/Button";
-import Label from "@/components/ui/Label";
+'use client'
+import { useState } from "react";
+import Checkbox from "@/components/ui/Checkbox";
 
 export default function Home() {
+  const [isChecked, setIsChecked] = useState(false);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-neutral-950">
-      <Label label="Label" tooltip="Tooltip" required={true} />
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-neutral-0">
+      <Checkbox size="sm" isChecked={isChecked} onChange={setIsChecked} />
     </div>
   );
 }
