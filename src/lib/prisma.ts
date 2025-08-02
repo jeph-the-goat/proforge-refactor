da85831e31340965fe66cfd3cf387bedb9602618
Jeph Tamuno <jephery@DESKTOP-4871JD8.webisoft.com>
2025-08-01 22:13:51 -0400

    fix: attempt db fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client"
+// Example from lib/db.ts
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma 
+export default prisma
